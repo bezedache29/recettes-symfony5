fededa3ea7dd70960d909491771f7eed9fcb9860
Christophe Salou <bezedache29@gmail.com>
2021-07-21 23:42:33 +0200

    Entity Aliment + migration

---
+++ src/Entity/Aliment.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AlimentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AlimentRepository::class)
+ */
+class Aliment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $calories;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $proteines;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $lipides;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCalories(): ?int
+    {
+        return $this->calories;
+    }
+
+    public function setCalories(int $calories): self
+    {
+        $this->calories = $calories;
+
+        return $this;
+    }
+
+    public function getProteines(): ?float
+    {
+        return $this->proteines;
+    }
+
+    public function setProteines(float $proteines): self
+    {
+        $this->proteines = $proteines;
+
+        return $this;
+    }
+
+    public function getLipides(): ?float
+    {
+        return $this->lipides;
+    }
+
+    public function setLipides(float $lipides): self
+    {
+        $this->lipides = $lipides;
+
+        return $this;
+    }
+}
